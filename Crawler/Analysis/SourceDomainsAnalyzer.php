f478f178fb2f79a81e5db89909951816ac6e1c9f
jan.reges <jan.reges@siteone.cz>
2023-10-22 00:00:10 +0200

    analyzers: added source domains analyzer with summary of domains and downloaded content types (number/size/duration)

---
+++ Crawler/Analysis/SourceDomainsAnalyzer.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Crawler\Analysis;
+
+use Crawler\Components\SuperTable;
+use Crawler\Components\SuperTableColumn;
+use Crawler\Crawler;
+use Crawler\Options\Group;
+use Crawler\Options\Option;
+use Crawler\Options\Options;
+use Crawler\Options\Type;
+use Crawler\Utils;
+
+class SourceDomainsAnalyzer extends BaseAnalyzer implements Analyzer
+{
+
+    public function shouldBeActivated(): bool
+    {
+        return true;
+    }
+
+    public function analyze(): void
+    {
+        $stats = [];
+        foreach ($this->status->getVisitedUrls() as $visitedUrl) {
+            $urlHost = parse_url($visitedUrl->url, PHP_URL_HOST);
+
+            // create init stat item for the host
+            if (!isset($stats[$urlHost])) {
+                $stat = [
+                    'domain' => $urlHost,
+                ];
+                foreach (Crawler::getContentTypeIds() as $contentTypeId) {
+                    $stat["{$contentTypeId}.count"] = 0;
+                    $stat["{$contentTypeId}.totalSize"] = 0;
+                    $stat["{$contentTypeId}.totalExecTime"] = 0;
+                }
+                $stats[$urlHost] = $stat;
+            }
+
+            $hostStat = &$stats[$urlHost];
+            $contentTypeId = $visitedUrl->contentType;
+
+            $hostStat["{$contentTypeId}.count"]++;
+            $hostStat["{$contentTypeId}.totalSize"] += $visitedUrl->size;
+            $hostStat["{$contentTypeId}.totalExecTime"] += $visitedUrl->requestTime;
+        }
+
+        // convert $stats to $data in this format ['siteone.io' => ['total' => '15 / 253 kB / 0.84 s', 'html' => '3 / 843 kB / 0.28 s', 'js' => '4 / 56 kB / 0.54 s']]
+        $data = [];
+        $contentTypeLongestValue = [];
+        foreach ($stats as $domain => $stat) {
+            $total = [
+                'count' => 0,
+                'size' => 0,
+                'execTime' => 0,
+            ];
+            $data[$domain] = [
+                'domain' => $domain,
+                'totals' => $total,
+            ];
+            foreach (Crawler::getContentTypeIds() as $contentTypeId) {
+                $count = $stat["{$contentTypeId}.count"];
+                $size = $stat["{$contentTypeId}.totalSize"];
+                $execTime = $stat["{$contentTypeId}.totalExecTime"];
+                $contentType = Utils::getContentTypeNameById($contentTypeId);
+
+                $total['count'] += $count;
+                $total['size'] += $size;
+                $total['execTime'] += $execTime;
+
+                if ($count) {
+                    $data[$domain][$contentType] = str_replace(' ', '', "$count / " . Utils::getFormattedSize($size, 0) . " / " . number_format($execTime, 1, '.', ' ') . " s");
+                } else {
+                    $data[$domain][$contentType] = '';
+                }
+
+                $contentTypeLongestValue[$contentType] = max($contentTypeLongestValue[$contentType] ?? 0, strlen($data[$domain][$contentType]));
+            }
+            $data[$domain]['totals'] = str_replace(' ', '', "{$total['count']} / " . Utils::getFormattedSize($total['size'], 0) . " / " . number_format($total['execTime'], 1, '.', ' ') . " s");
+            $data[$domain]['totalCount'] = $total['count'];
+        }
+
+        // unset all content-type columns that have 0 requests in total
+        foreach ($data as $domain => $stat) {
+            foreach ($contentTypeLongestValue as $contentTypeName => $longestValue) {
+                if ($longestValue === 0) {
+                    unset($data[$domain][$contentTypeName]);
+                }
+            }
+        }
+
+        // setup supertable
+        $superTableColumns = [
+            new SuperTableColumn('domain', 'Domain'),
+            new SuperTableColumn('totals', 'Totals'),
+        ];
+
+        // add content-type columns
+        foreach ($data as $domain => $stat) {
+            foreach ($stat as $contentType => $value) {
+                if ($contentType === 'domain' || $contentType === 'totals' || $contentType === 'totalCount') {
+                    continue;
+                }
+                $superTableColumns[] = new SuperTableColumn($contentType, $contentType);
+            }
+            break;
+        }
+
+        $superTable = new SuperTable(
+            'source-domains-summary',
+            "Source domains summary",
+            "No source domains found.",
+            $superTableColumns, false, 'totalCount', 'DESC'
+        );
+
+        $superTable->setData($data);
+        $this->status->addSuperTableAtBeginning($superTable);
+        $this->output->addSuperTable($superTable);
+    }
+
+    public function getOrder(): int
+    {
+        return 205;
+    }
+
+    public static function getOptions(): Options
+    {
+        return new Options();
+    }
+}
