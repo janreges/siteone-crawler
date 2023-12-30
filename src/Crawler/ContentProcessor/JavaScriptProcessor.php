aec6cab051a46df9d89866f5cfd7e66312dafb92
jan.reges <jan.reges@siteone.cz>
2023-12-30 17:31:22 +0100

    javascript processor: extract JS urls also from imports like import {xy} from "./path/foo.js"

--- src/Crawler/ContentProcessor/JavaScriptProcessor.php
+++ src/Crawler/ContentProcessor/JavaScriptProcessor.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 namespace Crawler\ContentProcessor;
 
 use Crawler\Crawler;
+use Crawler\FoundUrl;
 use Crawler\FoundUrls;
 use Crawler\ParsedUrl;
 
@@ -26,7 +27,7 @@ class JavaScriptProcessor extends BaseProcessor implements ContentProcessor
      */
     public function findUrls(string $content, ParsedUrl $sourceUrl): ?FoundUrls
     {
-        return null;
+        return $this->findUrlsImportFrom($content, $sourceUrl);
     }
 
     /**
@@ -35,6 +36,35 @@ class JavaScriptProcessor extends BaseProcessor implements ContentProcessor
     public function applyContentChangesForOfflineVersion(string &$content, int $contentType, ParsedUrl $url): void
     {
         $content = str_ireplace('crossorigin', '_SiteOne_CO_', $content);
+    }
+
+    /**
+     * Find URLs in JavaScript import from statements
+     * Example JS content ...import{R as W}from"./Repl.209fef3e.js";...
+     *
+     * @param string $content
+     * @param ParsedUrl $sourceUrl
+     * @return FoundUrls|null
+     */
+    private function findUrlsImportFrom(string $content, ParsedUrl $sourceUrl): ?FoundUrls
+    {
+        if (!str_contains($content, 'from')) {
+            return null;
+        }
+
+        preg_match_all('/from\s*["\']([^"\']+\.js[^"\']*)["\']/i', $content, $matches);
+        $foundUrlsTxt = [];
+        foreach ($matches[1] ?? [] as $match) {
+            $foundUrlsTxt[] = trim($match);
+        }
+
+        if (!$foundUrlsTxt) {
+            return null;
+        }
+
+        $foundUrls = new FoundUrls();
+        $foundUrls->addUrlsFromTextArray($foundUrlsTxt, $sourceUrl->path, FoundUrl::SOURCE_JS_URL);
+        return $foundUrls;
     }
 
 }
